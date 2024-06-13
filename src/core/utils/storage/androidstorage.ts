52e262bb737c81dc2e1cabd5a9b0e9907ebc0e2b
andi <iandy2233@gmail.com>
2024-06-13 13:53:24 +0800

    improve code

--- src/core/utils/storage/androidstorage.ts
+++ src/core/utils/storage/androidstorage.ts
@@ -1,32 +1,66 @@
-
+import { getGlobalThis } from '@offirmo/globalthis-ponyfill'
 
 /**
  * @internal
  */
-//TODO: 
+//TODO: now for testing
 export const androidStore =  {
   setItem: async (key: string, value: unknown): Promise<void> => {
-    localStorage.setItem(key, value as string);
+
+    const globalThis = getGlobalThis();
+
+    if(globalThis.nulink == null || typeof globalThis.nulink === 'undefined')
+    {
+        globalThis.nulink = {}
+    }
+    
+    globalThis.nulink[key] = value as string;
+
   },
 
   getItem: async (key: string): Promise<any> => {
-    return localStorage.getItem(key);
+
+    const globalThis = getGlobalThis();
+    
+    if(globalThis != null && typeof globalThis === 'undefined' && (globalThis as any).nulink != null && typeof  (globalThis as any).nulink !== 'undefined')
+    {
+      return (globalThis as any).nulink[key];
+    }
+    return null;    
   },
   removeItem: async (key: string): Promise<void> => {
     try {
-      localStorage.removeItem(key);
+      const globalThis = getGlobalThis();
+
+      if(globalThis.nulink == null || typeof globalThis.nulink === 'undefined')
+      {
+          globalThis.nulink = {}
+      }
+
+      globalThis.nulink[key] = undefined;
+
+      //delete globalThis.nulink[key]
+
+      //localStorage.removeItem(key);
+
     } catch (error) {
       console.info(`removeItem: ${key}`);
     }
   },
 
   removeItems: async (keys: string[]): Promise<void> => {
+    if(globalThis.nulink == null || typeof globalThis.nulink === 'undefined')
+      {
+          globalThis.nulink = {}
+      }
+
     for (const key of keys) {
-      localStorage.removeItem(key);
+      globalThis.nulink[key] = undefined;
+      //localStorage.removeItem(key);
     }
   },
 
   clear: async (): Promise<void> => {
-    localStorage.clear();
+    globalThis.nulink = {}
   },
 };
--- src/core/utils/storage/index.ts
+++ src/core/utils/storage/index.ts
@@ -4,12 +4,20 @@
 Therefore, the interactive page is put into components, the exposed message or interface distribution is put into api / route, which is exported by this file (index.ts), and api is the basic packaging interface.
 Only exposed messages are exported here, and functions that are directly called cannot be exported, otherwise the front end can be called at will.
 */
-  
-import { androidStore } from "./androidstorage";
 
-export { androidStore } from "./androidstorage";
+import { androidStore } from './androidstorage'
+import { localStore } from './localstorage'
+import { iosStore } from './iosstorage'
+
+export { androidStore } from './androidstorage'
+export { localStore } from './localstorage'
+export { iosStore } from './iosstorage'
+
+const bandroidStore = process.env.REACT_APP_STORAGE === 'android'
+const biosStore = process.env.REACT_APP_STORAGE === 'ios'
+// const blocalStore = process.env.REACT_APP_STORAGE === 'localstorage'
 
 /**
  * @internal
  */
-export const store = androidStore;
+export const store = bandroidStore ? androidStore : biosStore ? iosStore : localStore;
